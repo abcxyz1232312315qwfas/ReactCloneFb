5694cb7e30dfcd78786adc9acc7e4f9b95dc1f31
ducdeptrai1999 <53177120+ducdeptrai1999@users.noreply.github.com>
2020-06-30 15:39:11 +0700

    Update header.js

--- src/component/header.js
+++ src/component/header.js
@@ -3,22 +3,23 @@ import React from 'react';
 function Header() {
   return (
       <>
-        <div className="headerx">
-        </div>
-        <div className="header">
-            <div id="img1" className= "header" >
-                <img src="facebook.png"/>
-            </div>
+        <div className="headerx"></div>
+          <form>
+            <div className="header">
+                <div id="img1" className= "header" >
+                    <img src="facebook.png"/>
+                </div>
                 <div id="form1" className= "header" >Email hoặc điện thoại
-                    <input id="headerbox" type="mail" name="name" /><br />
+                        <input id="headerbox" type="mail"/><br />
                 </div>
                 <div id="form2" className="header">
-                    Password<br />
-                    <input id="headerbox" type="password" name="password" /><br />
-                    Quên tài khoản?
+                        Password<br />
+                        <input id="headerbox" type="password"/><br />
+                        Quên tài khoản?
                 </div>
-                <input type="submit" className="submit1" value="login"/>
-            </div>
+                <input type="submit" className="submit1" value="Đăng Nhập"/>
+              </div>
+            </form>
         </>
   );
 }
