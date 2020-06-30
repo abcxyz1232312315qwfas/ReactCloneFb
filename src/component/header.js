81bc48b59bff2648d7bea1c1ea2ff7c8c243130a
buiminhduc1999 <53177120+buiminhduc1999@users.noreply.github.com>
2020-06-30 18:08:46 +0700

    Update header.js

--- src/component/header.js
+++ src/component/header.js
@@ -1,26 +1,40 @@
 import React from 'react';
 
+function HeaderImage() {
+  return (
+    <>
+      <div id="img1" className="header" >
+        <img src="facebook.png" />
+      </div>
+    </>
+  );
+}
+function HeaderLogin() {
+  return (
+    <>
+      <div id="form1" className="header" >Email hoặc điện thoại
+          <input id="headerbox" type="mail" /><br />
+      </div>
+      <div id="form2" className="header">
+        Password<br />
+        <input id="headerbox" type="password" /><br />
+          Quên tài khoản?
+      </div>
+      <input type="submit" className="submit1" value="Đăng Nhập" />
+    </>
+  );
+}
 function Header() {
   return (
-      <>
-        <div className="headerx"></div>
-          <form>
-            <div className="header">
-                <div id="img1" className= "header" >
-                    <img src="facebook.png"/>
-                </div>
-                <div id="form1" className= "header" >Email hoặc điện thoại
-                        <input id="headerbox" type="mail"/><br />
-                </div>
-                <div id="form2" className="header">
-                        Password<br />
-                        <input id="headerbox" type="password"/><br />
-                        Quên tài khoản?
-                </div>
-                <input type="submit" className="submit1" value="Đăng Nhập"/>
-              </div>
-            </form>
-        </>
+    <>
+      <div className="headerx"></div>
+      <form>
+        <div className="header">
+          <HeaderImage></HeaderImage>
+          <HeaderLogin></HeaderLogin>
+        </div>
+      </form>
+    </>
   );
 }
 
