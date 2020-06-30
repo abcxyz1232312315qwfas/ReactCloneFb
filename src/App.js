ac099fd165d1f9b164a1ad150ae8c83f9bde3113
ducdeptrai1999 <53177120+ducdeptrai1999@users.noreply.github.com>
2020-06-30 15:35:01 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,26 +4,25 @@ import './App.css';
 import Header from './component/header';
 import Main from './component/main';
 import Footer from './component/footer';
-import Full from './component/full';
-
+import _ from 'lodash';
 const emailRegex = RegExp(
   /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 );
 
 const formValid = ({ formErrors, ...rest }) => {
-  let valid = true;
+      let valid = true;
 
-  // validate form errors being empty
-  Object.values(formErrors).forEach(val => {
-    val.length > 0 && (valid = false);
-  });
+      // validate form errors being empty
+      Object.values(formErrors).forEach(val => {
+        val.length > 0 && (valid = false);
+      });
 
-  // validate the form was filled out
-  Object.values(rest).forEach(val => {
-    val === null && (valid = false);
-  });
+      // validate the form was filled out
+      Object.values(rest).forEach(val => {
+        val === null && (valid = false);
+      });
 
-  return valid;
+      return valid;
 };
 
 class App extends React.Component {
@@ -91,7 +90,6 @@ class App extends React.Component {
       default:
         break;
     }
-
     this.setState({formErrors, [name]:value}, ()=> console.log(this.state));
   }
   render(){
@@ -99,23 +97,7 @@ class App extends React.Component {
   return (
 
     <div className="wapper">
-         <div className="headerx"></div>
-          <form>
-            <div className="header">
-                <div id="img1" className= "header" >
-                    <img src="facebook.png"/>
-                </div>
-                <div id="form1" className= "header" >Email hoặc điện thoại
-                        <input id="headerbox" type="mail"/><br />
-                </div>
-                <div id="form2" className="header">
-                        Password<br />
-                        <input id="headerbox" type="password"/><br />
-                        Quên tài khoản?
-                </div>
-                <input type="submit" className="submit1" value="login"/>
-              </div>
-            </form>
+         <Header></Header>
 
           <form onSubmit={this.handleSubmit} noValidate>
           <div class="content">
@@ -125,98 +107,89 @@ class App extends React.Component {
                 <div id="img2" class= "content" ><img src="world.png"/></div>
                 <div id="intro3" class="content">Nhanh chóng và dễ dàng.</div>
                 <div id="form3" class="content">
-                  <div className="firstName">
-                      <input  className={formErrors.firstName.length > 0 ? "error" : null}
-                              placeholder="Họ" 
-                              type="text" 
-                              id="namebox" 
-                              name="firstName" 
-                              onChange={this.handleChange} 
-                              noValidate 
-                      />
-                      <div id='message1'>
-                      {formErrors.firstName.length > 0 && (
-                        <span className="errorMessage">{formErrors.firstName}</span>
-                      )}
-                      </div>
-                  </div>
-                  <div className="lastName">
-                      <input  className={formErrors.lastName.length > 0 ? "error" : null}
-                              placeholder="Tên" 
-                              type="text" id="namebox" 
-                              name="lastName" 
-                              onChange={this.handleChange} 
-                              noValidate
-                      />
-                      <div id='message1'>
-                      {formErrors.lastName.length > 0 && (
-                        <span className="errorMessage">{formErrors.lastName}</span>
-                      )}
-                      </div>
+                <div className="username">
+                  <div className="container">
+                        <input  className={formErrors.firstName.length > 0 ? "error" : null}
+                                placeholder="Họ" 
+                                type="text" 
+                                id="namebox" 
+                                name="firstName" 
+                                onChange={this.handleChange} 
+                                noValidate 
+                        />
+                        <div id='message1'>
+                          {formErrors.firstName.length > 0 && (
+                            <span className="errorMessage">{formErrors.firstName}</span>
+                          )}
+                        </div>
+                    </div>
                   </div>
                   <div className="email">
-                      <input  className={formErrors.email.length > 0 ? "error" : null}
-                              placeholder="Số điện thoại hoặc email" 
-                              type="text" 
-                              id="mailbox" 
-                              name="email" 
-                              onChange={this.handleChange} 
-                              noValidate
-                      />
-                      <div id='message1'>
-                      {formErrors.email.length > 0 && (
-                          <span className="errorMessage">{formErrors.email}</span>
-                        )}
+                    <div className="container">
+                          <div>
+                            <input  className={formErrors.lastName.length > 0 ? "error" : null}
+                                    placeholder="Tên" 
+                                    type="text" id="namebox" 
+                                    name="lastName" 
+                                    onChange={this.handleChange} 
+                                   noValidate
+                             />
+                          </div>
+                        <div id='message1'>
+                           {formErrors.lastName.length > 0 && (
+                            <span className="errorMessage">{formErrors.lastName}</span>
+                          )}
                         </div>
+                    </div>
                   </div>
-                  <div className="password">
-                      <input  className={formErrors.password.length > 0 ? "error" : null}
-                              placeholder="Mật khẩu mới" 
-                              id="mailbox2" 
-                              type="password" 
-                              name="password" 
-                              onChange={this.handleChange} 
-                              noValidate
-                      />
-                      <div id='message1'>
-                      {formErrors.password.length > 0 && (
-                        <span className="errorMessage">{formErrors.password}</span>
-                      )}
+                  <div className="email">
+                    <div className="container">
+                          <div>
+                              <input  className={formErrors.email.length > 0 ? "error" : null}
+                                      placeholder="Số điện thoại hoặc email" 
+                                      type="text" 
+                                      id="mailbox" 
+                                      name="email" 
+                                      onChange={this.handleChange} 
+                                      noValidate
+                              />
+                            </div>
+                            <div id='message1'>
+                                {formErrors.email.length > 0 && (
+                                  <span className="errorMessage">{formErrors.email}</span>
+                                )}
+                              </div>
                       </div>
-                  </div>
+                    </div>
+                    <div className="password">
+                      <div className="container">
+                          <div>
+                            <input  className={formErrors.password.length > 0 ? "error" : null}
+                                    placeholder="Mật khẩu mới" 
+                                    id="mailbox2" 
+                                    type="password" 
+                                    name="password" 
+                                    onChange={this.handleChange} 
+                                    noValidate
+                            />
+                          </div>
+                          <div id='message1'>
+                              {formErrors.password.length > 0 && (
+                                <div className="errorMessage">{formErrors.password}</div>
+                              )}
+                          </div>
+                      </div>
+                    </div>
                     <br/><br/>
                     Ngày sinh<br/>
                         <select name="day" id="selectday">
-                            {/* <script type="text/javascript">
-                                for (var index = 0; index < 30; index++) {
-                                    document.write('<option value="#">'+i+'</option>')
-                                }
-                            </script> */}
-                            <option value="#">1</option>
-                            <option value="#">2</option>
-                            <option value="#">3</option>
-                            <option value="#">4</option>
-                            <option value="#">5</option>
-                            <option value="#">6</option>
-                            <option value="#">...</option>
+                            {_.range(1, 30 + 1).map(value => <option key={value} value={value}>{value}</option>)}
                         </select>
                         <select name="month" id="selectmonth">
-                            <option value="#">Tháng 1</option>
-                            <option value="#">Tháng 2</option>
-                            <option value="#">Tháng 3</option>
-                            <option value="#">Tháng 4</option>
-                            <option value="#">Tháng 5</option>
-                            <option value="#">Tháng 6</option>
-                            <option value="#">...</option>
+                            {_.range(1, 12 + 1).map(value => <option key={value} value={value}>{'Thang '+value}</option>)}
                         </select>
                         <select name="year" id="selectday">
-                            <option value="#">1995</option>
-                            <option value="#">1996</option>
-                            <option value="#">1997</option>
-                            <option value="#">1998</option>
-                            <option value="#">1999</option>
-                            <option value="#">2000</option>
-                            <option value="#">...</option>
+                            {_.range(1970, 2020 + 1).map(value => <option key={value} value={value}>{value}</option>)}
                         </select>
                     <br/>
                     <br/>
@@ -235,9 +208,6 @@ class App extends React.Component {
             </div>
           </form>
           <Footer></Footer>
-        {/* <Header/>,
-       <Main/>,
-       <Footer/>  */}
     </div>
   );
   }
